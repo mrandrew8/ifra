f1b1885f47dd18f55037259b5625079f9627ae44
mrandrew8 <nikolaevandronik@yandex.ru>
2024-01-02 17:23:05 +0300

    создал provider.tf и вынес в него из old_main.tf блок provider. Параметризовал значения token, cloud_id, folder_id и сделал возможность задавать их через terraform.tfvars

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,7 +1,7 @@
 provider "yandex" {
-  cloud_id  = "b1g3jddf4nv5e9okle7p"
-  folder_id = "b1gk067d0krcal09flv4"
-  zone      = "ru-central1-a"
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.zone
 }
 
     variable "network_zone" {
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,3 @@
+cloud_id  = "b1g3jddf4nv5e9okle7p"
+folder_id = "b1gk067d0krcal09flv4"
+zone      = "ru-central1-a"
